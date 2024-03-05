1f4cd9352c04ccf74acc36c77a5ef3b9c7fe23f5
gyun <akf151568@gmail.com>
2024-03-05 15:20:02 +0900

    2024-03-05 : Solve LeetCode 2문제

---
+++ src/main/java/leetcode/Array_String/JumpGame2_45.java
@@ -0,0 +1,18 @@
+package leetcode.Array_String;
+
+class JumpGame2_45 {
+    public int jump(int[] nums) {
+        int maxStep = nums[0];
+        int maxPos = nums[0];
+        if(nums.length < 2) return 0;
+        int jumps = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(maxStep < i){
+                jumps++;
+                maxStep = maxPos;
+            }
+            maxPos = Math.max(maxPos, nums[i] + i);
+        }
+        return jumps;
+    }
+}
---
+++ src/main/java/leetcode/Array_String/JumpGame_55.java
@@ -0,0 +1,25 @@
+package leetcode.Array_String;
+
+class JumpGame_55 {
+    public boolean canJump(int[] nums) {
+
+        boolean[] possible = new boolean[nums.length];
+        possible[nums.length-1] = true;
+        int step = 1;
+        for(int i = nums.length-1; i > 0; i--){
+            if(possible[i]){
+                if(nums[i-1] != 0) possible[i-1] = true;
+                else{
+                    step++;
+                }
+            }else{
+                if(nums[i-1] - step < 0) step++;
+                else{
+                    step = 1;
+                    possible[i-1] = true;
+                }
+            }
+        }
+        return possible[0];
+    }
+}
