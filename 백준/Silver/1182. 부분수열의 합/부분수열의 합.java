f47d458827fdedf0af550c93cc3346ac87d93d3b
yigyun <70265528+yigyun@users.noreply.github.com>
2025-01-26 22:24:29 +0900

    [Silver II] Title: 부분수열의 합, Time: 112 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -3,34 +3,36 @@ import java.io.*;
 
 class Main{
 
+    static int count;
+    static int[] nums;
     static int n;
     static int s;
-    static int[] nums;
-    static int result;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
         nums = new int[n];
-        result = 0;
+        count = 0;
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        find(0, 0);
-        if(s == 0) result--;
-        System.out.print(result);
+        found(0, 0);
+        if(s == 0) count--;
+        System.out.print(count);
     }
 
-    static void find(int index, int sum){
-        if(sum == s) result++;
-
+    static void found(int index, int c){
+        if(c == s){
+            count++;
+        }
         for(int i = index; i < n; i++){
-            find(i + 1, sum + nums[i]);
+            found(i + 1, c + nums[i]);
         }
     }
 }
