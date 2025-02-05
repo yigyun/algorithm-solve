5a6f74a0d29e49e2d28445da2420a83b71cbe8b6
yigyun <70265528+yigyun@users.noreply.github.com>
2025-02-05 12:44:41 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 23200 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -3,31 +3,34 @@ import java.io.*;
 
 class Main{
     public static void main(String[] args) throws Exception{
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int S = sc.nextInt();
-        
-        int[] nums = new int[N];
-        for(int i = 0; i < N; i++){
-            nums[i] = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-                
-        int start = 0;
+
+        int sta = 0;
         int end = 0;
-        int length = N + 1;
-        int sum = 0;
-        
-        while(end < N){
-            sum += nums[end];
-            end++;
-                while(sum >= S){
-                    length = Math.min(end - start, length);
-                    sum -= nums[start];
-                    start++;
-                }
-         
+        int sum = nums[0];
+        int min = Integer.MAX_VALUE;
+
+        while(sta <= end && end < n){
+            if(sum >= s){
+                min = Math.min(min, end - sta);
+                sum -= nums[sta];
+                sta++;
+            }else{
+                end++;
+                if(end < n) sum += nums[end];
+            }
         }
-        if(length == N + 1) length = 0;
-        System.out.print(length);
+
+        System.out.print(min == Integer.MAX_VALUE ? 0 : min + 1);
     }
 }
