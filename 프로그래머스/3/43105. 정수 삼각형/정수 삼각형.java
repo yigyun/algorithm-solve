ef09a83c1f495a8f774e10bd149f58756365d439
yigyun <70265528+yigyun@users.noreply.github.com>
2025-02-10 16:21:39 +0900

    [level 3] Title: 정수 삼각형, Time: 10.89 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,26 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        int[][] dp = new int[triangle.length][];
-        for(int i = 0; i < triangle.length; i++){
-            dp[i] = Arrays.copyOf(triangle[i], triangle[i].length);
+        
+        int length = triangle.length;
+                
+        // 추가 메모리 안써보기
+        for(int x = 1; x < length; x++){
+            int n = triangle[x-1].length;
+            for(int y = 0; y < triangle[x].length; y++){
+                int find = 0;
+                if(y - 1 >= 0 && y - 1 < n){
+                    find = triangle[x-1][y-1];
+                }
+                if(y >= 0 && y < n){
+                    find = Math.max(find, triangle[x-1][y]);
+                }
+                triangle[x][y] += find;
+            }
         }
         
-        int max = 0;
-        for(int i = 1; i < dp.length; i++){
-            for(int j = 0; j < dp[i].length; j++){
-                if(j < dp[i-1].length)
-                    dp[i][j] = Math.max(dp[i][j], triangle[i-1][j] + triangle[i][j]);
-                if(j - 1 >= 0)
-                    dp[i][j] = Math.max(dp[i][j], triangle[i-1][j-1] + triangle[i][j]);
-            }
-            for(int j = 0; j < dp[i].length; j++){
-                triangle[i][j] = dp[i][j];
-                max = Math.max(max, dp[i][j]);
-            }
+        int max = -1;
+        for(int i = 0; i < triangle[length - 1].length; i++){
+            max = Math.max(triangle[length - 1][i], max);
         }
+        
         return max;
     }
 }
