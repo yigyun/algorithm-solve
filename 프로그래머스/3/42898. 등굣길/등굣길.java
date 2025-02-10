d5e1a48d1e5cebcf05b34ff2cc2fdaca6608797c
yigyun <70265528+yigyun@users.noreply.github.com>
2025-02-10 16:55:44 +0900

    [level 3] Title: 등굣길, Time: 2.93 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,42 +1,37 @@
-import java.util.*;
-
-// 최단 경로의 개수
 class Solution {
     
-    static int[] dx = new int[]{0, 1};
-    static int[] dy = new int[]{1, 0};
+    static int[] dx = {1, 0}, dy = {0, 1};
     
-    public int solution(int m, int n, int[][] puddles) {
+    public int solution(int n, int m, int[][] puddles) {
         int answer = 0;
-        int MOD = 1000000007;
         
-        int[][] dp = new int[n][m];
+        int[][] map = new int[m][n];
+        map[0][0] = 1;
         
         for(int i = 0; i < puddles.length; i++){
             int x = puddles[i][1] - 1;
             int y = puddles[i][0] - 1;
-            dp[x][y] = -1;
+            map[x][y] = -1;
         }
         
-        dp[0][0] = 1;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(dp[i][j] == -1){
-                    dp[i][j] = 0;
-                    continue;
-                }
-                
-                if(i > 0){
-                    dp[i][j] = (dp[i][j] + dp[i-1][j]) % MOD;
-                }
-                if(j > 0){
-                    dp[i][j] = (dp[i][j] + dp[i][j-1]) % MOD;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(map[i][j] == -1) continue;
+                for(int dir = 0; dir < 2; dir++){
+                    int nx = i + dx[dir];
+                    int ny = j + dy[dir];
+                    if(isRange(m, n, nx, ny) && map[nx][ny] != -1){
+                        map[nx][ny] = (map[nx][ny] + map[i][j]) % 1000000007;
+                    }
                 }
             }
         }
         
-        return dp[n-1][m-1];
+        return map[m-1][n-1];
     }
-
+    
+    static boolean isRange(int m, int n, int nx, int ny){
+        return !(nx < 0 || nx >= m || ny < 0 || ny >= n);
+    }
+    
 }
