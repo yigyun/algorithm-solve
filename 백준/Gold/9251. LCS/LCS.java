fcafe3169d8683698a99b61bc5232dabcd4e4b9c
yigyun <70265528+yigyun@users.noreply.github.com>
2025-02-13 14:55:29 +0900

    [Gold V] Title: LCS, Time: 136 ms, Memory: 18580 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int n = str1.length();
+        int m = str2.length();
+        
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        
+        System.out.print(dp[n][m]);
+    }
+}
